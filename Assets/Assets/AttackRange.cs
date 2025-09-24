339c0e5aac0aaeba03fd5a491b75c0a2a98518ad
JangHyeonChul <wkdgus1136@naver.com>
2025-09-24 23:41:02 +0900

    Tower 공격 구조 변경

--- Assets/Assets/AttackRange.cs
+++ Assets/Assets/AttackRange.cs
@@ -7,27 +7,13 @@ using UnityEngine;
 
 public class AttackRange : MonoBehaviour
 {
-    private Collider2D attackRangeCollider;
+    private List<GameObject> enemyPoolList = new();
 
-    private List<GameObject> enemyPoolList;
-    private Coroutine attackCoroutine;
-
-    public GameObject basicAttack;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        attackRangeCollider = GetComponent<Collider2D>();
-        if (attackRangeCollider == null)
-        {
-            Debug.Log("Attack Collider NPE");
-        }
 
-        enemyPoolList = new List<GameObject>();
-        attackCoroutine = StartCoroutine(TowerAttack());
     } 
 
-    // Update is called once per frame
     void Update()
     {
         
@@ -44,34 +30,15 @@ public class AttackRange : MonoBehaviour
 
     void OnTriggerExit2D(Collider2D other)
     {
-        enemyPoolList.Remove(other.gameObject);
+        // 적이 공격 사정거리 내에 들어왔을 경우 삭제
+        if (other.CompareTag("Enemy")) 
+        { 
+            enemyPoolList.Remove(other.gameObject);
+        }
     }
 
-    private IEnumerator TowerAttack()
+    public List<GameObject> GetEnemyList()
     {
-        while (true)
-        {
-            if (enemyPoolList.Count > 0)
-            {
-                // Attack the first enemy in the list
-                GameObject targetEnemy = enemyPoolList[0];
-                BasicAttack.Instance.Attack(targetEnemy);
-                //basicAttack.gameObject.SetActive(true);
-                //Vector3 attackPosition = basicAttack.transform.position;
-                //Vector3 targetPosition = targetEnemy.transform.position;
-
-                //Vector3 attackMovePosition = attackPosition - targetPosition;
-
-                //Vector3 direction = attackMovePosition.normalized;
-                //basicAttack.transform.position = direction * 10f;
-
-                //int towerAttackPower = TowerManager.Instance.GetTowerAttackPower();
-                
-                //targetEnemy.GetComponent<EnemyHealth>().TakeDamage(towerAttackPower, targetEnemy);
-
-            }
-
-            yield return new WaitForSeconds(1f);
-        }
+        return enemyPoolList;
     }
 }
