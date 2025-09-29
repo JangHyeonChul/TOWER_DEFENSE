617afc14ec0bc06573a64da376f03eacfb394088
JangHyeonChul <wkdgus1136@naver.com>
2025-09-29 20:58:45 +0900

    Spell Panel 추가

--- Assets/Assets/UIManager.cs
+++ Assets/Assets/UIManager.cs
@@ -14,6 +14,7 @@ public class UIManager : MonoBehaviour
     public Button shopExitBtn;
 
     public Button attackUpradeBtn;
+    public GameObject spellPanel;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
